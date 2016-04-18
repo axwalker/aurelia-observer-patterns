2a515a98be5dfd4b29050b01ca2eb57600456724
Andrew Walker <awalkerdev@gmail.com>
2016-04-18 21:42:57 +0100

    events triggered through event aggregator

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,23 @@
-import {bindable} from 'aurelia-framework';
+import {autoinject, bindable} from 'aurelia-framework';
+import {EventAggregator} from 'aurelia-event-aggregator';
 
 import {Person} from './person';
 
 
+@autoinject
 export class App {
     @bindable() people: Person[] = [
         {firstName: 'John', lastName: 'Doe'},
         {firstName: 'Joe', lastName: 'Bloggs'},
     ];
+    eventAggregator: EventAggregator;
+    
+    constructor(eventAggregator: EventAggregator) {
+        this.eventAggregator = eventAggregator;
+        this.eventAggregator.subscribe('person:selected', person => this.personSelected(person));
+    }
+    
+    personSelected(person: Person) {
+        alert(`${person.firstName} ${person.lastName} selected`);
+    }
 }
--- src/person-list.ts
+++ src/person-list.ts
@@ -1,8 +1,19 @@
-import {bindable} from 'aurelia-framework';
+import {autoinject, bindable} from 'aurelia-framework';
+import {EventAggregator} from 'aurelia-event-aggregator';
 
 import {Person} from './person';
 
 
+@autoinject
 export class PersonList {
     @bindable() people: Person[];
+    eventAggregator: EventAggregator;
+    
+    constructor(eventAggregator: EventAggregator) {
+        this.eventAggregator = eventAggregator;
+    }
+    
+    selectPerson(person: Person) {
+        this.eventAggregator.publish('person:selected', person);
+    }
 }
