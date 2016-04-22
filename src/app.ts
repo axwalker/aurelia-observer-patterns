bad98f15dcc87ff0240b78c587c8856e1f5c5a37
atsu85 <atsu85@gmail.com>
2016-04-22 22:56:39 +0300

    trigger event using setter callback

--- src/app.ts
+++ src/app.ts
@@ -6,4 +6,8 @@ export class App {
         {firstName: 'John', lastName: 'Doe'},
         {firstName: 'Joe', lastName: 'Bloggs'},
     ];
+
+    selectPerson(person) {
+        alert(`${person.firstName} ${person.lastName} selected`);
+    }
 }
--- src/person-list.ts
+++ src/person-list.ts
@@ -5,4 +5,9 @@ import {Person} from './person';
 
 export class PersonList {
     @bindable() people: Person[];
+    @bindable() selectPersonCallback: (Person) => void;
+
+    selectPerson(person) {
+        this.selectPersonCallback(person);
+    }
 }
